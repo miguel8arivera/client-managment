2a7fa6c4bbbead3849e657ce86435858683718f0
Miguel Angel Ochoa <miguel.angel.8a.rivera@gmail.com>
2023-01-07 09:10:44 -0500

    NewCustomerPage file worked in views

--- src/pages/CustomerPage.tsx
+++ src/pages/CustomerPage.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 import Customer from '../components/Customer';
 import { customers } from '../data/customers';
 
@@ -16,11 +16,20 @@ type PropCliente = {
 
 const CustomerPage: FC = function () {
   const dataCustomers: PropCliente[] = useLoaderData();
+  const navigate = useNavigate();
 
   return (
     <div>
       <h2 className="font-black text-4xl text-blue-900"> Customers</h2>
       <p className="mt-4">Custumers Managment</p>
+      <div className="flex justify-end py-4">
+        <button
+          className="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 "
+          onClick={() => navigate('/new/customer')}
+        >
+          Add Customer
+        </button>
+      </div>
       {dataCustomers.length ? (
         <table className="w-full bg-white shadow mt-5 table-auto">
           <thead className="bg-blue-700 text-white">
--- src/pages/NewCustomerPage.tsx
+++ src/pages/NewCustomerPage.tsx
@@ -1,9 +1,26 @@
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NewCustomerPage: FC = function () {
+  const navigate = useNavigate();
   return (
     <div>
-      <h2> Pag de clientes nuevos renderizado por un Outtlet</h2>
+      <h2 className="font-black text-4xl text-blue-900">New Customers</h2>
+      <p className="mt-4">Add a new customer</p>
+      <div className="flex justify-end">
+        <button
+          type="button"
+          className="text-white bg-blue-700 font-bold uppercase hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          onClick={() => navigate('/')}
+        >
+          Return to
+        </button>
+      </div>
+      <div className="bg-white shadow rounded-md md:w-4/4 max-auto px-5 py-10">
+        <p className="text-center font-bold text-blue-900 uppercase">
+          Register Customer
+        </p>
+      </div>
     </div>
   );
 };
