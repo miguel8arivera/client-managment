4c176167e2c852f24ae4f4636def223fa2f6b9a1
Miguel Angel Ochoa <miguel.angel.8a.rivera@gmail.com>
2023-01-08 10:35:45 -0500

    CustomerPageForm.tsx style to completed and react-icons added in project

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { TiMail, TiPhone } from 'react-icons/ti';
 
 const Customer: FC = function ({ customer }) {
   const { name, company, email, id, phone } = customer;
@@ -9,12 +10,14 @@ const Customer: FC = function ({ customer }) {
       </td>
       <td className="p-6 text-center">
         <p className="text-gray-600 ">
-          {' '}
-          <span className="text-gray-800  font-semibold">{email}</span>
+          <span className=" flex flex-row justify-center  text-gray-800  font-semibold">
+            <TiMail className="mr-2" /> {email}
+          </span>
         </p>
         <p className="text-gray-600 ">
-          {' '}
-          <span className="text-gray-800 uppercase font-semibold">{phone}</span>
+          <span className="flex flex-row justify-center text-gray-800 uppercase font-semibold">
+            <TiPhone className="mr-2" /> {phone}
+          </span>
         </p>
       </td>
       <td className="p-6 flex gap-4 ">
--- src/components/CustomerPageForm.tsx
+++ src/components/CustomerPageForm.tsx
@@ -4,67 +4,18 @@ const CustomerPageForm: FC = function () {
   return (
     <>
       <div className="mb-4">
-        <label className="text-gray-800" htmlFor="nombre">
-          Nombre:
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="username"
+        >
+          Name:
         </label>
         <input
-          id="nombre"
+          className="shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="name"
           type="text"
-          className="mt-2 block w-full p-3 bg-blue-50"
-          placeholder="Nombre del Cliente"
-          name="nombre"
-        />
-      </div>
-      <div className="mb-4">
-        <label className="text-gray-800" htmlFor="empresa">
-          Empresa:
-        </label>
-        <input
-          id="empresa"
-          type="text"
-          className="mt-2 block w-full p-3 bg-blue-50"
-          placeholder="Empresa del Cliente"
-          name="empresa"
-        />
-      </div>
-
-      <div className="mb-4">
-        <label className="text-gray-800" htmlFor="email">
-          E-mail:
-        </label>
-        <input
-          id="email"
-          type="email"
-          className="mt-2 block w-full p-3 bg-blue-50"
-          placeholder="Email del Cliente"
-          name="email"
-        />
-      </div>
-
-      <div className="mb-4">
-        <label className="text-gray-800" htmlFor="telefono">
-          Teléfono:
-        </label>
-        <input
-          id="telefono"
-          type="tel"
-          className="mt-2 block w-full p-3 bg-blue-50"
-          placeholder="Teléfono del Cliente"
-          name="telefono"
-        />
-      </div>
-
-      <div className="mb-4">
-        <label className="text-gray-800" htmlFor="notas">
-          Notas:
-        </label>
-        <textarea
-          as="textarea"
-          id="notas"
-          type="text"
-          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-          placeholder="Notas del Cliente"
-          name="notas"
+          placeholder="name"
+          name="name"
         />
       </div>
       <div className="mb-4">
@@ -72,35 +23,50 @@ const CustomerPageForm: FC = function () {
           className="block text-gray-700 text-sm font-bold mb-2"
           htmlFor="username"
         >
-          Username
+          Company:
         </label>
         <input
           className="shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username"
+          id="company"
           type="text"
-          placeholder="Username"
+          placeholder="company"
+          name="company"
         />
       </div>
-
-      <div>
+      <div className="mb-4">
         <label
-          htmlFor="about"
-          className="block text-sm font-medium text-gray-700"
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="username"
         >
-          About
+          E-mail:
         </label>
-        <div className="mt-1">
-          <textarea
-            id="about"
-            name="about"
-            rows={3}
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-            placeholder="you@example.com"
-          ></textarea>
-        </div>
-        <p className="mt-2 text-sm text-gray-500">
-          Brief description for your profile. URLs are hyperlinked.
-        </p>
+        <input
+          className="shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="email"
+          type="email"
+          placeholder="name12@gmail.com"
+          name="email"
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="username"
+        >
+          Phone:
+        </label>
+        <input
+          className="shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="phone"
+          type="text"
+          placeholder="+54 9 11 1234-5678"
+          name="ph"
+        />
+      </div>
+      <div className="flex justify-center ">
+        <button className="bg-indigo-600 p-2 font-bold uppercase text-white hover:bg-blue-700 w-full rounded-md">
+          Register
+        </button>
       </div>
     </>
   );
